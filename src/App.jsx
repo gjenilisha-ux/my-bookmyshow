6a93799e2908efa9b5fc4f06b6d00a5a1cebcf74
gjenilisha-ux <gjenilisha@gmail.com>
2026-08-31 22:15:52 +0530

    Update image source path for Baahubali 2

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const movies = [
     title: "Baahubali 2",
     genre: "Action • Fantasy",
     image:
-      <img src="/my-bookmyshow/images/baahubali2.jpg" />
+     <img src="public/images/baahubali2.jpg" />
   },
   {
     title: "Pathu Thala",
